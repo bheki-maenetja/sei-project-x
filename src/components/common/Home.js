07315d34d7960e81059a74602d75a1cc41104107
bheki-maenetja <bhekimaenetja@gmail.com>
2020-02-04 11:38:32 +0000

    changed name

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div className="hero is-dark is-fullheight-with-navbar">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title is-1">Cosa Nostra</h1>
+        <h1 className="title is-1">Mob Jobs</h1>
         <h1 className="subtitle is-1">It&apos;s what it is</h1>
       </div>
     </div>
